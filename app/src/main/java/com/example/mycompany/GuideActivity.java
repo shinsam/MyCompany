1ff5afde44ab80ed2edd307ffc2d89d3169dc21a
shinsam <shin321gg@gmail.com>
2022-05-30 10:27:01 +0900

    call 작업

--- app/src/main/java/com/example/mycompany/GuideActivity.java
+++ app/src/main/java/com/example/mycompany/GuideActivity.java
@@ -2,13 +2,27 @@ package com.example.mycompany;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class GuideActivity extends AppCompatActivity {
+public class GuideActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide);
+        //ImageView img_map = findViewById(R.id.img_map);
+        //img_map.setOnClickListener(this);
+        findViewById(R.id.img_map).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW,
+                Uri.parse("geo:37.4807426,127.084375?z=15" ));
+        startActivity(intent);
     }
 }
